c4c4c6272f4937c2b87202dabe70f57ac82694eb
Michael Chen <michaelchen592@gmail.com>
2024-04-23 08:55:40 -0400

    Clubs template

--- clubsite/clubs/views.py
+++ clubsite/clubs/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello world!")
+
+def clubs(request):
+    template = loader.get_template('clubs.html')
+    return HttpResponse(template.render({}, request))
