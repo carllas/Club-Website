4c3b6d5d9089c64dab8abf047c8e8c47c995f918
AlexL173 <alexliu13579@gmail.com>
2024-05-01 08:13:22 -0400

    m,n,,

--- clubsite/clubs/urls.py
+++ clubsite/clubs/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='homepage'),
-    path('clubs/', views.clubs, name='clubs')
+    path('clubs/', views.clubs, name='clubs'),
+    path('clubs/details/<int:id>', views.details, name='details'),
+
 ]
